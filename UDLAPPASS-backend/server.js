0321387a96898255dedb1dea19f6517c88073592
DavidMR15 <dav.medray@gmail.com>
2026-04-21 18:11:28 -0600

    correcta llamada del env

--- UDLAPPASS-backend/server.js
+++ UDLAPPASS-backend/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
