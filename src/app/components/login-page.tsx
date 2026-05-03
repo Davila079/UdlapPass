b47271592b381ff7decbd419c3708ffafc69bc9b
DavidMR15 <dav.medray@gmail.com>
2026-05-03 12:48:41 -0600

    Cypress automated test for login

--- src/app/components/login-page.tsx
+++ src/app/components/login-page.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "./auth-context";
 import { useNavigate } from "react-router";
-//import imgUdlap1 from "figma:asset/94454e9a7e3e91be5322a669fe39012d8be17a6a.png";
 
 export function LoginPage() {
   const [userId, setUserId] = useState("");
@@ -11,15 +10,15 @@ export function LoginPage() {
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
-  e.preventDefault();
-  setError("");
-  const success = await login(userId, password);
-  if (success) {
-    navigate("/home");
-  } else {
-    setError("ID o contraseña incorrectos");
-  }
-};
+    e.preventDefault();
+    setError("");
+    const success = await login(userId, password);
+    if (success) {
+      navigate("/home");
+    } else {
+      setError("ID o contraseña incorrectos");
+    }
+  };
 
   return (
     <div className="min-h-screen bg-[#f1f1f1] flex flex-col items-center px-6">
@@ -37,6 +36,7 @@ export function LoginPage() {
             Usuario / ID
           </label>
           <input
+            data-cy="usuario"
             type="text"
             value={userId}
             onChange={(e) => setUserId(e.target.value)}
@@ -50,6 +50,7 @@ export function LoginPage() {
             Contrasena
           </label>
           <input
+            data-cy="password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -59,7 +60,9 @@ export function LoginPage() {
         </div>
 
         {error && (
-          <p className="text-red-500 text-sm text-center">{error}</p>
+          <p data-cy="error-msg" className="text-red-500 text-sm text-center">
+            {error}
+          </p>
         )}
 
         <p className="text-right font-['DM_Serif_Text',serif] text-[14px] text-[#737373] cursor-pointer">
@@ -67,6 +70,7 @@ export function LoginPage() {
         </p>
 
         <button
+          data-cy="submit"
           type="submit"
           className="w-full h-[50px] bg-[#f48a32] rounded-lg font-['DM_Serif_Text',serif] text-[18px] text-white mt-4 active:opacity-80"
         >
@@ -74,7 +78,7 @@ export function LoginPage() {
         </button>
 
         <p className="text-center text-[#737373] text-xs mt-4">
-          Usuarios demo: 182634 / 400562 / 300100 / 500200 (pass: 1234)
+          Usuarios demo: 183112 / 1 / 2 / 183913 / (pass: password123)
         </p>
       </form>
     </div>
